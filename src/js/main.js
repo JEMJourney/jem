048fd0b87884270d90eafbfdb47b2d77b1da32bd
JEMJourney <jem.journey@gmail.com>
2016-03-24 14:54:52 +0100

    clean code, content system
    move javascript code

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,27 @@
-$(".opener").on("click", function() {
-	console.log("opener click");
+jQuery(window).load(function() {
+
+	$(".opener").click(function() {
+		$(".menu").animate({"left":"0px"}, "fast");
+	});
+
+	$(".closer").click(function() {
+		$(".menu").animate({"left":"-1000px"}, "fast");
+	});
+
+	$(".mobile-search").click(function() {
+		if($(".content-search").css("display") == "inline") {
+
+		} else {
+			$(".content-search").css("display", "inline");
+			$(".opener").css("display", "none");
+			$(".brand-search").css("display", "inline");
+		}
+	});
+
+	$(".brand-search").click(function() {
+		$(".opener").css("display", "inline");
+		$(".brand-search").css("display", "none");
+		$(".content-search").css("display", "none");
+	});
+	
 });
